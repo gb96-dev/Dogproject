a3e62014c2c60ff34752761cba62fcf8f009a24a
김규범 <gb24404156@gmail.com>
2025-12-12 19:17:03 +0900

    Dog 클래스 캡슐화 적용 및 bark/introduce 메서드 추가

--- src/Dog.java
+++ src/Dog.java
@@ -1,10 +1,38 @@
 public class Dog {
-    String name;
-    String breed;
+    private String name;
+    private String breed;
 
     // 생성자
     public Dog(String name, String breed) {
         this.name = name;
+        this.breed = breed;
+    }
+
+    // 직접 만든 메서드 1: 짖기
+    public void bark() {
+        System.out.println(name + "가(이) 멍멍 짖었어!");
+    }
+
+    // 직접 만든 메서드 2: 자기소개
+    public String introduce() {
+        return "안녕! 나는 " + name + "이고, 품종은 " + breed + "야!";
+    }
+
+    // getter 메서드
+    public String getName() {
+        return name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    // setter 메서드 (필요하면 사용)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String breed) {
         this.breed = breed;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        // 첫 번째 강아지: 이름은 멍멍이, 종은 말티즈
+
+        // 강아지 3마리 생성
         Dog dog1 = new Dog("멍멍이", "말티즈");
-        System.out.println("첫 번째 강아지 - 이름: " + dog1.name + ", 종: " + dog1.breed);
-
-        // 두 번째 강아지: 이름은 초코, 종은 진돗개
         Dog dog2 = new Dog("초코", "진돗개");
-        System.out.println("두 번째 강아지 - 이름: " + dog2.name + ", 종: " + dog2.breed);
-
-        // 세 번째 강아지: 이름은 복치, 종은 푸들
         Dog dog3 = new Dog("복치", "푸들");
-        System.out.println("세 번째 강아지 - 이름: " + dog3.name + ", 종: " + dog3.breed);
+
+        // 직접 만든 메서드 호출
+        dog1.bark();
+        dog2.bark();
+        dog3.bark();
+
+        System.out.println();
+
+        // introduce() 메서드 호출
+        System.out.println(dog1.introduce());
+        System.out.println(dog2.introduce());
+        System.out.println(dog3.introduce());
+
+        System.out.println();
+
+        // getter 이용 출력
+        System.out.println("이름: " + dog1.getName() + ", 품종: " + dog1.getBreed());
+        System.out.println("이름: " + dog2.getName() + ", 품종: " + dog2.getBreed());
+        System.out.println("이름: " + dog3.getName() + ", 품종: " + dog3.getBreed());
     }
 }
